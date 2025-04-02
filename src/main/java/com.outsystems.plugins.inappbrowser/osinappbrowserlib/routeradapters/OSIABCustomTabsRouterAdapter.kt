ad230a4d4db064cbd89f7fb15d45dfb6a17c1cf9
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2025-04-02 16:57:45 +0100

    RMET-4121 :: Remove unused callback from CustomTabsRouter (#44)
    * chore: Remove unused callback from CustomTabsRouter
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-4121
    
    * chore: Update CHANGELOG

--- src/main/java/com.outsystems.plugins.inappbrowser/osinappbrowserlib/routeradapters/OSIABCustomTabsRouterAdapter.kt
+++ src/main/java/com.outsystems.plugins.inappbrowser/osinappbrowserlib/routeradapters/OSIABCustomTabsRouterAdapter.kt
@@ -25,7 +25,6 @@ class OSIABCustomTabsRouterAdapter(
     flowHelper: OSIABFlowHelperInterface,
     onBrowserPageLoaded: () -> Unit,
     onBrowserFinished: () -> Unit,
-    onBrowserPageNavigationCompleted: (String?) -> Unit,
     private val customTabsSessionHelper: OSIABCustomTabsSessionHelperInterface = OSIABCustomTabsSessionHelper(),
 ) : OSIABBaseRouterAdapter<OSIABCustomTabsOptions, Boolean>(
     context = context,
@@ -33,8 +32,7 @@ class OSIABCustomTabsRouterAdapter(
     options = options,
     flowHelper = flowHelper,
     onBrowserPageLoaded = onBrowserPageLoaded,
-    onBrowserFinished = onBrowserFinished,
-    onBrowserPageNavigationCompleted = onBrowserPageNavigationCompleted
+    onBrowserFinished = onBrowserFinished
 ) {
 
     private val browserId = UUID.randomUUID().toString()
@@ -197,9 +195,6 @@ class OSIABCustomTabsRouterAdapter(
                     startCustomTabsControllerActivity(true)
                     onBrowserFinished()
                     eventsJob?.cancel()
-                }
-                is OSIABEvents.BrowserPageNavigationCompleted -> {
-                    onBrowserPageNavigationCompleted(event.url)
                 }
                 else -> {}
             }
