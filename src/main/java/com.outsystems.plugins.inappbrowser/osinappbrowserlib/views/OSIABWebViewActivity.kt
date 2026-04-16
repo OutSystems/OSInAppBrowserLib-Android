eb1cfff21e44f6d902bc7852adc8992f9ec38dda
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-16 18:15:56 +0100

    refactor: extract listener to separate method

--- src/main/java/com.outsystems.plugins.inappbrowser/osinappbrowserlib/views/OSIABWebViewActivity.kt
+++ src/main/java/com.outsystems.plugins.inappbrowser/osinappbrowserlib/views/OSIABWebViewActivity.kt
@@ -219,28 +219,6 @@ class OSIABWebViewActivity : AppCompatActivity() {
 
         setupWebView()
 
-        webView.setDownloadListener { url, _, contentDisposition, mimeType, _ ->
-            Log.e("downloadlistener", "mimeType=$mimeType, contentDisposition=$contentDisposition url=$url")
-            if ((mimeType == "application/pdf" || contentDisposition.contains(".pdf")) && !url.startsWith(PDF_VIEWER_URL_PREFIX)) {
-                lifecycleScope.launch(Dispatchers.IO) {
-                    val pdfFile = try {
-                        OSIABPdfHelper.downloadPdfToCache(this@OSIABWebViewActivity, url)
-                    } catch (_: IOException) {
-                        null
-                    }
-                    if (pdfFile != null) {
-                        withContext(Dispatchers.Main) {
-                            webView.stopLoading()
-                            originalUrl = url
-                            val pdfJsUrl =
-                                PDF_VIEWER_URL_PREFIX + Uri.encode("file://${pdfFile.absolutePath}")
-                            webView.loadUrl(pdfJsUrl)
-                        }
-                    }
-                }
-            }
-        }
-
         if (urlToOpen != null) {
             handleLoadUrl(urlToOpen, customHeaders)
             showLoadingScreen()
@@ -332,6 +310,14 @@ class OSIABWebViewActivity : AppCompatActivity() {
                 options.showURL && options.showToolbar
             )
         webView.webChromeClient = customWebChromeClient()
+
+        webView.setDownloadListener { url, _, contentDisposition, mimeType, _ ->
+            handleWebViewDownload(
+                url = url,
+                mimeType = mimeType,
+                contentDisposition = contentDisposition
+            )
+        }
     }
 
     /**
@@ -349,6 +335,38 @@ class OSIABWebViewActivity : AppCompatActivity() {
      */
     private fun customWebChromeClient(): WebChromeClient {
         return OSIABWebChromeClient()
+    }
+
+    /**
+     * Implement the WebKit DownloadListener and handle downloading and previewing PDF files
+     */
+    private fun handleWebViewDownload(
+        url: String?,
+        mimeType: String?,
+        contentDisposition: String?
+    ) {
+        if ((mimeType == "application/pdf" || (contentDisposition?.contains(".pdf") == true)) &&
+            (!url.isNullOrEmpty() && !url.startsWith(PDF_VIEWER_URL_PREFIX))
+        ) {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val pdfFile = try {
+                    OSIABPdfHelper.downloadPdfToCache(this@OSIABWebViewActivity, url)
+                } catch (_: IOException) {
+                    // can happen if we try to press the "save" button in pdf viewer
+                    //  which returns a blob url that we won't be able to download
+                    null
+                }
+                if (pdfFile != null) {
+                    withContext(Dispatchers.Main) {
+                        webView.stopLoading()
+                        originalUrl = url
+                        val pdfJsUrl =
+                            PDF_VIEWER_URL_PREFIX + Uri.encode("file://${pdfFile.absolutePath}")
+                        webView.loadUrl(pdfJsUrl)
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -819,7 +837,7 @@ class OSIABWebViewActivity : AppCompatActivity() {
         if (!showNavigationButtons) {
             navigationView.removeView(nav)
         } else defineNavigationButtons(isLeftRight, content)
-        
+
         if (!showURL) navigationView.removeView(urlText)
         else defineURLView(url, showNavigationButtons, navigationView, toolbarPosition, isLeftRight)
 
